b81e4d9c4eb37381fcb03d4786e3a28e27372b8f
francovici <francovici@users.noreply.github.com>
2022-11-04 16:49:16 -0300

    added arguments in help

--- src/help.js
+++ src/help.js
@@ -9,11 +9,11 @@ module.exports = () => {
 
     console.log('Command\t\tAction\t\t\t\t\tArguments');
 
-    console.log('pl --init\tInit project structure\t\t\t\t');
-    console.log('pl --clean\tCleans builded packages\t\t\t\t');
-    console.log('pl --build\tBuilds package from source files\t\t\t\t');
-    console.log('pl --compile\tCompiles existing packages on Oracle\t\t\t\t');
-    console.log('pl --help\tShows this help information\t\t\t\t');
-    console.log('pl --version\tShows version information\t\t\t\t');
+    console.log('pl --init\tInit project structure\t\t\t');
+    console.log('pl --clean\tCleans builded packages\t\t\t--package');
+    console.log('pl --build\tBuilds package from source files\t--package');
+    console.log('pl --compile\tCompiles existing packages on Oracle\t--package');
+    console.log('pl --help\tShows this help information\t\t\t');
+    console.log('pl --version\tShows version information\t\t\t');
 
 }
