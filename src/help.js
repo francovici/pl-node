2a53a1b46c8f16cae9ee88f784931a87f514d86e
francovici <francovici@users.noreply.github.com>
2022-11-04 17:15:13 -0300

    Created init function. Completed help

--- src/help.js
+++ src/help.js
@@ -9,11 +9,21 @@ module.exports = () => {
 
     console.log('Command\t\tAction\t\t\t\t\tArguments');
 
-    console.log('pl --init\tInit project structure\t\t\t');
-    console.log('pl --clean\tCleans builded packages\t\t\t--package');
-    console.log('pl --build\tBuilds package from source files\t--package');
-    console.log('pl --compile\tCompiles existing packages on Oracle\t--package');
+    console.log('pl --init\tInit project structure\t\t\tsubDirectory');
+    console.log('pl --clean\tCleans builded packages\t\t\tpackageName');
+    console.log('pl --build\tBuilds package from source files\tpackageName');
+    console.log('pl --compile\tCompiles existing packages on Oracle\tpackageName');
     console.log('pl --help\tShows this help information\t\t\t');
     console.log('pl --version\tShows version information\t\t\t');
+
+    console.log('');
+    console.log('Usage Examples:');
+    console.log('');
+    console.log('pl --init ' + clc.blue('outDir'));
+    console.log('Starts project structure within an "outDir" folder inside the current directory');
+    
+    console.log('');
+    console.log('pl --build ' + clc.blue('MY_PACKAGE'));
+    console.log('Looks for the source code inside ./Packages/"MY_PACKAGE" directory and builds the package.');
 
 }
--- src/interface.js
+++ src/interface.js
@@ -1,13 +1,15 @@
 import arg from "arg";
-import help from './help'
-
+import help from './help';
+import init from './init';
 
 export function interfaceCommand(args) {
-    console.log(args);
     const actions = argumentOptionsParser(args);
 
     if(actions.help){
         help();
+    }
+    if(actions.init){
+        init(actions.parameters[0]);
     }
 }
 
@@ -31,10 +33,11 @@ function argumentOptionsParser(rawArguments) {
     );
 
     return {
-      init: args._[0],
+      init: args["--init"] || false,
       help: args["--help"] || false,
       build: args["--build"] || false,
       clean: args["--clean"] || false,
       compile: args["--compile"] || false,
+      parameters: rawArguments.slice(3)
     };
   }
