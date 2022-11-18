af3cb9436d1b44e71a3f559b7f2f04f93b40e1be
francovici <k39997@frro.utn.edu.ar>
2022-11-18 00:00:13 -0300

    created projectFactory

---
+++ src/helpers/projectFactory.js
@@ -0,0 +1,55 @@
+
+const clc = require('cli-color');
+var path = require('path');
+const fs = require("fs");
+const { exit } = require('process');
+
+/* Getting pl-node root dir from any working directory */
+let arrPath = path.parse(__dirname).dir.split(path.sep);
+arrPath.pop();
+const binaryPath = path.join(...arrPath);
+
+function createProjectOnDir(dirPath){
+    
+    console.log('');
+    console.log( clc.black(clc.bgBlue('--- CREATING PROJECT ---')));
+    console.log('');
+    console.log('On: ' + path.resolve(`${dirPath}`))
+    console.log('');
+    
+    createProjectFolder(dirPath,'');
+    createProjectFolder(dirPath,'Packages');
+    createProjectFolder(dirPath,'src');
+    createProjectFolder(dirPath,'tests');
+    copyProjectAsset(path.resolve(`${binaryPath}/assets/env`),dirPath,'.env'); 
+    copyProjectAsset(path.resolve(`${binaryPath}/assets/gitignore`),dirPath,'.gitignore'); 
+}
+
+function createProjectFolder(dir,name,_options){
+    try{
+        fs.mkdirSync(path.resolve(`${dir}${name}`),_options);
+        if(name){
+            console.log(clc.greenBright('/'+name) + ' created');
+        }
+    }
+    catch(err){
+        if (!err === 'EEXIST'){
+            throw err;
+        }
+        else{
+            if(name){
+                console.log(clc.blueBright('/'+name) + ' already exists. Skipped.');
+            }
+        }
+    }
+}   
+function createProjectFile(dir,name,_contents,_options){
+    fs.writeFileSync(path.resolve(`${dir}${name}`),_contents,_options); 
+    console.log(clc.greenBright(name) + ' created');
+}   
+function copyProjectAsset(assetPath,destinationDir,newName){
+    fs.copyFileSync(assetPath,path.resolve(`${destinationDir}${newName}`));
+    console.log(clc.greenBright(newName) + ' created');
+}   
+
+module.exports = {copyProjectAsset, createProjectFile, createProjectFolder, createProjectOnDir}
