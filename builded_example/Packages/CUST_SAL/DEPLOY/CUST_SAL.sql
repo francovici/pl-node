82253f892c3bb515076a52baad34ae833f9165e6
francovici <k39997@frro.utn.edu.ar>
2022-11-19 19:29:20 -0300

    updated examples for testing purposes

---
+++ builded_example/Packages/CUST_SAL/DEPLOY/CUST_SAL.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE PACKAGE cust_sal AS 
+   PROCEDURE find_sal(c_id customers.id%type); 
+END cust_sal;
+
+CREATE OR REPLACE PACKAGE BODY cust_sal AS  
+   
+   PROCEDURE find_sal(c_id customers.id%TYPE) IS 
+   c_sal customers.salary%TYPE; 
+   BEGIN 
+      SELECT salary INTO c_sal 
+      FROM customers 
+      WHERE id = c_id; 
+      dbms_output.put_line('Salary: '|| c_sal); 
+   END find_sal; 
+END cust_sal; 
