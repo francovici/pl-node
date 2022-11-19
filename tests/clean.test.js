0b42fe3c2d54f3d8240e5bb677a5b0a85e8f04a6
francovici <k39997@frro.utn.edu.ar>
2022-11-19 19:29:58 -0300

    clean function initial tests

---
+++ tests/clean.test.js
@@ -0,0 +1,44 @@
+const clean = require('../src/clean');
+const fs = require('fs');
+const path = require('path');
+const {expect} = require('chai');
+const exampleDirPath = './builded_example/Packages';
+const testExampleDirPath = './Packages';
+
+beforeEach('Rebuilding Packages dir for next test...',() => {
+    fs.cpSync(exampleDirPath,testExampleDirPath, { recursive: true });
+});
+
+describe('pl --clean', () => {
+    it('should clean all builded packages', (done) => {
+        clean()
+        .then(() => {
+            const cust_sal_deployed_dir_exists = fs.existsSync(path.resolve(`${testExampleDirPath}/CUST_SAL/DEPLOY`));
+            const emp_bonus_deployed_dir_exists = fs.existsSync(path.resolve(`${testExampleDirPath}/EMP_BONUS/DEPLOY`));
+            expect((!cust_sal_deployed_dir_exists && !emp_bonus_deployed_dir_exists)).to.be.true;
+            return done();
+        })
+        .catch((err) => {
+            done(err);
+        });
+    })
+});
+
+describe('pl --clean <PACKAGE_NAME>', () => {
+    it('should clean only CUST_SAL package', (done) => {
+        clean('CUST_SAL')
+        .then(() => {
+            const cust_sal_deployed_dir_exists = fs.existsSync(path.resolve(`${testExampleDirPath}/CUST_SAL/DEPLOY`));
+            const emp_bonus_deployed_dir_exists = fs.existsSync(path.resolve(`${testExampleDirPath}/EMP_BONUS/DEPLOY`));
+            expect(!cust_sal_deployed_dir_exists && emp_bonus_deployed_dir_exists).to.be.true;
+            return done();
+        })
+        .catch((err) => {
+            done(err);
+        });
+    })
+});
+
+/* after(() => {
+    fs.rmSync(testExampleDirPath, { recursive: true });
+}); */
